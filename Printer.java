844c7807b11cf2c811419996ddd51f586dc6ee40
Bawrer <84140652+Bawrer@users.noreply.github.com>
2021-05-14 14:44:25 +0200

    Add files via upload

---
+++ Printer.java
@@ -0,0 +1,13 @@
+//Printer.java
+
+import javax.swing.*;
+public class Printer{
+
+public static void main(String []args )
+{
+JOptionPane.showMessageDialog(null,"Programming is fun");
+System.exit(0);
+}
+
+
+}
